1af00b9e224f84b728d44377713d250dc50d24c4
JayCodes <www.amujonathan@gmail.com>
2025-11-05 18:21:48 +0100

    fix: change Number to lowercase.

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -2,7 +2,7 @@ const args = process.argv.slice(2);
 const num = args[0];
 
 if (isNaN(num)) {
-    console.log('Not a Number')
+    console.log('Not a number')
 } else {
     console.log('My number: '+ num);
 }
