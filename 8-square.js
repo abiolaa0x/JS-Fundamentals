3cbd306704dd83d48b696b1310facb3d64255150
JayCodes <www.amujonathan@gmail.com>
2025-11-07 03:31:21 +0100

    fix: changed Size in log to lowercase.

--- 8-square.js
+++ 8-square.js
@@ -2,7 +2,7 @@ const arg = process.argv[2];
 let sqr = parseInt(arg);
 
 if (isNaN(sqr)) {
-  console.log("Missing Size");
+  console.log("Missing size");
 } else {
   for (let i = 0; i < sqr; i++) {
     let row = "";
